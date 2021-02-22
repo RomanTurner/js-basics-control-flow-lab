89c8b757906bd061da115f35434decb2c651f6b1
Roman Turner <rn.turner@live.com>
2021-02-22 11:37:24 -0800

    commit tree f5bf54f7b578d888ee10908df3b56d878013974f

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(n) {
+  if (n < 400) {
+    return "This one is on me!";
+  }
+  if (n > 2000 && n < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  if (n >= 2500) {
+    return "No can do.";
+  }
+}
+console.log(scuberGreetingForFeet(199));
+
+function ternaryCheckCity(city) {
+return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (true) {
+
+    case tip === 'generous':
+      return "Thank you so much.";
+      break;
+
+    case tip === 'not as generous':
+      return "Thank you.";
+      break;
+
+    default:
+      return "Bye.";
+      break;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+console.log(switchOnCharmFromTip('generous'));
